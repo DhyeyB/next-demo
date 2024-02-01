632c7933d17f6af7f46bb882552d9f2e7dd64c9e
bs-dhyey-bhattasana <dhyey.bhattasana@bombaysoftwares.com>
2024-02-01 16:41:38 +0530

    test

---
+++ diff.sh
@@ -0,0 +1,5 @@
+if git diff --quiet --exit-code HEAD^ HEAD -- pages/; then
+            echo "No changes in 'pages/' directory. Skipping conditional job."
+          else
+            echo "Changes detected in 'pages/' directory. Running conditional job."
+          fi
