6460c37d530122f4aa90960074e90f37eebfe363
bs-dhyey-bhattasana <dhyey.bhattasana@bombaysoftwares.com>
2024-02-27 12:34:23 +0530

    update android script

--- android.sh
+++ android.sh
@@ -1,11 +1,15 @@
+sudo apt update
+sudo apt install unzip
+
 echo "=========================================="
 echo "======== Java Installation steps ======="
 echo "=========================================="
 
-sudo apt install openjdk-8-jdk
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-echo $JAVA_HOME
+# sudo apt install openjdk-8-jdk
+sudo apt install openjdk-17-jre -Y
+# export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+# export PATH=$JAVA_HOME/bin:$PATH
+# echo $JAVA_HOME
 java -version
 
 echo "=========================================="
@@ -16,12 +20,16 @@ echo "=========================================="
 wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
 ls
 unzip commandlinetools-linux-11076708_latest.zip -d android-sdk
-ls android-sdk
-ls android-sdk/cmdline-tools
-ls android-sdk/cmdline-tools/bin
+cd android-sdk
+mkdir latest
+mv  bin/ latest/
+mv lib/ latest/
+mv source.properties latest/
+mv NOTICE.txt latest/
+cd latest
 pwd
 export ANDROID_HOME=/home/runner/work/next-demo/next-demo/android-sdk
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools:$ANDROID_HOME/cmdline-tools/bin
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools:$ANDROID_HOME/cmdline-tools/latest/bin
 
 echo "=========================================="
 echo "======== sdkmanager configuration ======="
@@ -30,5 +38,3 @@ echo "=========================================="
 sdkmanager --list
 yes | sdkmanager --licenses
 sdkmanager "platform-tools" "platforms;android-33" "build-tools;30.0.3" "emulator" "system-images;android-33;default;x86_64"
-ls android-sdk
-ls android-sdk/cmdline-tools
